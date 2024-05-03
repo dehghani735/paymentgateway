79ee84ae313cdaa5c500058be70010ea5a93db03
mr.detafti <dehghani735@gmail.com>
2024-05-03 22:27:58 +0330

    Polished.

--- src/main/java/ir/snapppay/paymentgateway/service/PurchaseServiceImpl.java
+++ src/main/java/ir/snapppay/paymentgateway/service/PurchaseServiceImpl.java
@@ -1,7 +1,13 @@
 package ir.snapppay.paymentgateway.service;
 
 import ir.snapppay.paymentgateway.models.tables.records.PurchaseRecord;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
+@Slf4j
+@Service
+@AllArgsConstructor
 public class PurchaseServiceImpl implements PurchaseService {
 
     @Override
