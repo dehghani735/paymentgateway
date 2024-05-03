ded6d8462fa5aa5f094baa692a2b8c3bdb99b63a
mr.detafti <dehghani735@gmail.com>
2024-05-03 11:57:40 +0330

    first commit

---
+++ src/test/java/ir/snapppay/paymentgateway/PaymentgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.snapppay.paymentgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/ir/snapppay/paymentgateway/TestPaymentgatewayApplication.java
@@ -0,0 +1,23 @@
+package ir.snapppay.paymentgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestPaymentgatewayApplication {
+
+	@Bean
+	@ServiceConnection
+	PostgreSQLContainer<?> postgresContainer() {
+		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(PaymentgatewayApplication::main).with(TestPaymentgatewayApplication.class).run(args);
+	}
+
+}
